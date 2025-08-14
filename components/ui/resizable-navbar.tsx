8acd738e0c7996ba2af3145c127fca082d30d2eb
AmanVatsSharma <aman95026@gmail.com>
2025-08-14 12:39:47 +0530

    temory work around fix- navbar

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // This file is a client component and uses motion/react for animations
