7e2a0b3ee930d1ab77141a27bc8a4b43d566c94f
AmanVatsSharma <aman95026@gmail.com>
2025-08-14 11:11:52 +0530

    feat: add new dependencies and assets for 3D rendering
    - Added @react-three/rapier for physics support in 3D scenes.
    - Included meshline for advanced line rendering in 3D graphics.
    - Updated three.js version to 0.167.1 for compatibility.
    - Added new 3D model (card.glb) and image asset (lanyard.png) to the public directory.
    - Created global TypeScript declarations for .glb and .png modules, and for meshline components.

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,13 @@
 import { ThreeDMarqueeSection } from "@/components/home-page/hero-section";
 import Beams from "@/components/ui/beams-bg";
+import { Button } from "@/components/ui/button";
+import Lanyard from "@/components/ui/Lanyard/Lanyard";
 
 export default function AboutUsPage() {
     return (
         <div className="main w-full min-h-screen flex-col items-center justify-center gap-10">
             <ThreeDMarqueeSection />
+            <Lanyard/>
             <div className="text-center mt-10">
                 <h1 className="text-4xl font-bold mb-4">About Us</h1>
                 <p className="text-lg text-gray-700">
@@ -20,8 +23,14 @@ export default function AboutUsPage() {
                     speed={2}
                     noiseIntensity={1.75}
                     scale={0.2}
-                    rotation={0}
+                    rotation={45}
                 />
+                <p className="absolute bottom-1/2 left-1/2 transform -translate-x-1/2 text-white text-7xl font-semibold">
+                    Radient Beams for Creative User Interfaces
+                </p>
+                <Button className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white hover:bg-blue-600">
+                    Learn More
+                </Button>
             </div>
         </div>
     )
