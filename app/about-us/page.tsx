06ce7a6149285a207e746b8920959c83ba132932
AmanVatsSharma <aman95026@gmail.com>
2025-08-14 10:26:42 +0530

    feat(AboutUsPage): replace placeholder text with ThreeDMarqueeSection component; add ThreeDMarquee and GridLine components

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,9 @@
+import { ThreeDMarqueeSection } from "@/components/home-page/hero-section";
+
 export default function AboutUsPage() {
     return (
         <div className="main w-full min-h-screen flex items-center justify-center">
-            hello this is about us AboutUsPage.
+            <ThreeDMarqueeSection/>
         </div>
     )
 }
