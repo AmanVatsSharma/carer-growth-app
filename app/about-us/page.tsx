b6cadf7a27a8ad367ed38473eb390afdf5de88aa
AmanVatsSharma <aman95026@gmail.com>
2025-08-14 11:51:40 +0530

    refactor: comment out unused Lanyard component and related code for clarity

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,13 +1,13 @@
 import { ThreeDMarqueeSection } from "@/components/home-page/hero-section";
 import Beams from "@/components/ui/beams-bg";
 import { Button } from "@/components/ui/button";
-import Lanyard from "@/components/ui/Lanyard/Lanyard";
+// import Lanyard from "@/components/ui/Lanyard/Lanyard";
 
 export default function AboutUsPage() {
     return (
         <div className="main w-full min-h-screen flex-col items-center justify-center gap-10">
             <ThreeDMarqueeSection />
-            <Lanyard />
+            {/* <Lanyard /> */}
             <div className="text-center mt-10">
                 <h1 className="text-4xl font-bold mb-4">About Us</h1>
                 <p className="text-lg text-gray-700">
