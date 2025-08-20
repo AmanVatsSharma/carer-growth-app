dc36004924b0a4b80f377a155c17098a55c307f7
AmanVatsSharma <aman95026@gmail.com>
2025-08-20 14:23:10 +0530

    pictures updsated

--- components/home-page/popular-exams.tsx
+++ components/home-page/popular-exams.tsx
@@ -18,7 +18,7 @@ const exams = [
     features: ["Speaking", "Listening", "Reading", "Writing"],
     color: "from-blue-500/20 to-cyan-500/20",
     image:
-      "https://plus.unsplash.com/premium_photo-1714265046278-51dc8ee55405?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/pictures/ielts.jpg",
     avgScore: "6.5",
     passRate: "78%",
   },
@@ -33,7 +33,7 @@ const exams = [
     features: ["Academic Focus", "Internet-based", "Integrated Tasks"],
     color: "from-green-500/20 to-emerald-500/20",
     image:
-      "https://plus.unsplash.com/premium_photo-1714265046278-51dc8ee55405?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/pictures/TOEFL.webp",
     avgScore: "85",
     passRate: "72%",
   },
@@ -48,7 +48,7 @@ const exams = [
     features: ["Quantitative", "Verbal", "Analytical Writing"],
     color: "from-purple-500/20 to-violet-500/20",
     image:
-      "https://plus.unsplash.com/premium_photo-1714265046278-51dc8ee55405?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/pictures/gre.webp",
     avgScore: "310",
     passRate: "65%",
   },
@@ -63,7 +63,7 @@ const exams = [
     features: ["Data Insights", "Verbal", "Quantitative"],
     color: "from-orange-500/20 to-red-500/20",
     image:
-      "https://plus.unsplash.com/premium_photo-1714265046278-51dc8ee55405?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/pictures/GMAT-1.webp",
     avgScore: "650",
     passRate: "58%",
   },
